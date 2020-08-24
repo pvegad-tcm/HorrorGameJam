be224fa75a5872a2f9923e08a65e973b2ef6121a
RicardCabezas <r.cabezas.diez@gmail.com>
2020-08-24 12:25:44 +0200

    Rework of QTE to add fastclick and visual feedback

--- HorrorGameJam/Assets/Scripts/QuickTimeEvents/QTEView.cs
+++ HorrorGameJam/Assets/Scripts/QuickTimeEvents/QTEView.cs
@@ -4,14 +4,31 @@ using UnityEngine.UI;
 
 public class QTEView : MonoBehaviour
 {
+    [SerializeField] private PlayableDirector _animationTimeline;
+    [SerializeField] private PlayableAsset _keyPressedAnimation;    
+    [SerializeField] private Image _keyImage;
+    [SerializeField] private Animation _animationAura;
+    [SerializeField] private AnimationClip _animationFast;
+    [SerializeField] private AnimationClip _animationSlow;
+
     public PlayableDirector AnimationTimeline => _animationTimeline;
-    public Image KeyImage
+    public PlayableAsset KeyPressedAnimation => _keyPressedAnimation;
+    public Image KeyImage => _keyImage;
+
+    public void PlayBackgroundFast()
     {
-        get => _keyImage;
-        set => _keyImage = value;
+        _animationAura.clip = _animationFast;
+        _animationAura.Play();
+    }
+    
+    public void PlayBackgroundSlow()
+    {
+        _animationAura.clip = _animationSlow;
+        _animationAura.Play();
     }
 
-    [SerializeField] private PlayableDirector _animationTimeline;
-    [SerializeField] private Image _keyImage;
-
+    public void ActiveBackgroundAnimation(bool active)
+    {
+        _animationAura.gameObject.SetActive(active);
+    }
 }
