1017442d3ee3c5e4de74cf51b420955ea6ed07b8
Alan Elwood <alanelya@gmail.com>
2020-08-25 13:46:17 +0200

    Grab bucket

---
+++ HorrorGameJam/Assets/InteractableItemWithAnimation.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Playables;
+using VHS;
+
+namespace Interactions
+{
+    public class InteractableItemWithAnimation : InteractableBase
+    {
+        [SerializeField] private PlayableDirector _animationTimeline;
+
+        public override void OnInteract()
+        {
+            gameObject.layer = 0;
+            _animationTimeline.Play();
+        }
+    }
+}
