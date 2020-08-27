7a12a49eb1321c0d65f1fea2f37b814561c01cf8
PolVega <polvegadiaz@gmail.com>
2020-08-27 16:52:38 +0200

    New Interactable Item functionalities

--- HorrorGameJam/Assets/Scripts/Interactions/InteractableItem.cs
+++ HorrorGameJam/Assets/Scripts/Interactions/InteractableItem.cs
@@ -1,4 +1,3 @@
-using System.Collections;
 using System.Collections.Generic;
 using NaughtyAttributes;
 using TaskLists;
@@ -7,10 +6,8 @@ using VHS;
 
 namespace Interactions
 {
-    public class InteractableItem : InteractableBase
+    public abstract class InteractableItem : InteractableBase
     {
-        [SerializeField] private QTEInstaller _qteInstaller;
-        [SerializeField] private QuickTimeEventTemplate _template;
         [SerializeField] private TaskListView _taskListView;
 
         [BoxGroup("Task Lists")] [SerializeField] private TaskList _taskList;
@@ -18,26 +15,16 @@ namespace Interactions
         
         [Space] [ReorderableList] [SerializeField] 
         private List<InteractableItem> _itemsToActivateAfterInteract;
-        
+
         public override void OnInteract()
         {
-            gameObject.layer = (int)LayerValue.Default;
-            StartCoroutine(NextFrameInteraction());
+            gameObject.layer = (int) LayerValue.Default;
         }
 
         protected override void OnFinishedInteraction()
         {
             _taskListView.CheckTask(_taskList, _taskId);
             _itemsToActivateAfterInteract.ForEach(item => item.gameObject.layer = (int)LayerValue.Interactable);
-        }
-
-        private IEnumerator NextFrameInteraction()
-        {
-            yield return null;
-            _qteInstaller.Install(_template, OnFinishedInteraction);
-            
-            yield return null;
-            Destroy(this);
         }
     }
 }
