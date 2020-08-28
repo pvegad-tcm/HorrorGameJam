147eab0a4640d65474ff96e9be1694930f513b10
PolVega <polvegadiaz@gmail.com>
2020-08-28 14:28:31 +0200

    Interactable Item support GameObjects

--- HorrorGameJam/Assets/Scripts/Interactions/InteractableItem.cs
+++ HorrorGameJam/Assets/Scripts/Interactions/InteractableItem.cs
@@ -9,7 +9,8 @@ namespace Interactions
     public abstract class InteractableItem : InteractableBase
     {
         [SerializeField] private TaskListView _taskListView;
-
+        [SerializeField] private GameObject _soundObject;
+        
         [BoxGroup("Task Lists")] [SerializeField] private TaskList _taskList;
         [BoxGroup("Task Lists")] [SerializeField] private int[] _taskIds;
         
@@ -19,12 +20,14 @@ namespace Interactions
         public override void OnInteract()
         {
             SetLayer((int) LayerValue.Default);
+            _soundObject?.SetActive(true);
         }
 
         protected override void OnFinishedInteraction()
         {
             _taskListView.CheckTask(_taskList, _taskIds);
             _itemsToActivateAfterInteract.ForEach(item => item.SetLayer((int)LayerValue.Interactable));
+            _soundObject?.SetActive(false);
         }
     }
 }
